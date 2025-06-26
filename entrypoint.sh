70e2ceaf728bddf58a6b50b494fae66d5be4151f
Veiga <veiga3d@gmail.com>
2025-06-26 00:03:47 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-echo ">> Esperando Kafka Connect subir..."
-sleep 10
 
-for file in /connectors/*.json; do
-  echo ">> Registrando connector: $file"
-  curl -X POST -H "Content-Type: application/json" --data @"$file" http://localhost:8083/connectors
+# Inicia o Kafka Connect em background
+/kafka/bin/connect-distributed.sh /kafka/config/connect-distributed.properties &
+
+# Aguarda Kafka Connect REST API subir
+echo "Aguardando Kafka Connect REST API subir..."
+until curl -s http://localhost:8083/connectors; do
+  sleep 5
+  echo "Aguardando..."
 done
 
-# Executa o processo principal do Kafka Connect
-/etc/confluent/docker/run &
-child=$!
-wait "$child"
+echo "Kafka Connect está pronto. Deploy dos connectors..."
+
+# Faz o deploy dos arquivos JSON dos connectors
+for connector in /kafka/connectors/*.json; do
+  echo "Deploying $connector"
+  curl -X POST -H "Content-Type: application/json" --data @"$connector" http://localhost:8083/connectors || \
+  echo "O connector $connector já existe. Ignorando erro."
+done
+
+wait -n
+exit $?
